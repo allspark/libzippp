15c821b9ee11e5ea9af89035dfc8f07d8f611583
Dennis Börm <dennis.boerm@ise.de>
2022-09-20 10:21:45 +0200

    use find package libzip

--- cmake/libzipppConfig.cmake
+++ cmake/libzipppConfig.cmake
@@ -2,6 +2,6 @@ include(CMakeFindDependencyMacro)
 
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
 
-find_dependency(ZIP REQUIRED)
+find_dependency(libzip REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/libzipppTargets.cmake")
